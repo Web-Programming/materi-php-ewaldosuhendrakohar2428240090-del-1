33e7241bc60508dcf07f8ef5415513153271ef99
Wavy <ewaldosuhendrakohar_2428240090@mhs.mdp.ac.id>
2026-05-06 14:00:46 +0700

    Laravel

--- Laravel/app/Http/Controllers/ProductControler.php
+++ Laravel/app/Http/Controllers/ProductControler.php
@@ -1,20 +1,88 @@
 <?php
 
 namespace App\Http\Controllers;
-use illuminate\Http\Request;
-class ProductControler extends Controller{
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
     public function index()
     {
-        $products = [
-            ['id' => 1, 'Name ' => 'Laptop' 'Price' => 7500000]
-            ['id' => 2, 'Name ' => 'Mouse' 'Price' => 150000]
-            ['id' => 3, 'Name ' => 'keyboard' 'Price' => 300000]
-            ['id' => 4, 'Name ' => 'Monitor' 'Price' => 250000]
-        ];
-        return view('produk.index', compact('title','products'));
+        $title = "Daftar Produk";
+        // $products = [
+        //     ['id' => 1, 'name' => 'Laptop', 'price' => 7500000],
+        //     ['id' => 2, 'name' => 'Mouse', 'price' => 150000],
+        //     ['id' => 3, 'name' => 'Keyboard', 'price' => 300000],
+        //     ['id' => 4, 'name' => 'Monitor', 'price' => 2500000],
+        // ];
+        
+        //$products = Product::all(); //cara 1
+        //$products = DB::select('SELECT * FROM products'); //cara 2
+        $products = DB::table('products')->get(); //cara 3
 
+        return view('produk.index', compact('title', 'products'));
+        //return view('produk.index', [
+        //    'products' => $products, 
+        //    'title' => $title
+        //]);
     }
-    public function create(){
-        return view('Products.create');
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('produk.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $title = "Detail Produk";
+        $product = ['id' => $id, 'name' => 'Monitor', 'price' => 2500000];
+        return view('produk.detail', compact('id', 'product', 'title'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        return view('produk.edit', ['id' => $id]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+
+    function search(Request $request)
+    {
+        return view('produk.search');
     }
 }
--- Laravel/routes/web.php
+++ Laravel/routes/web.php
@@ -4,18 +4,17 @@ use Illuminate\Support\Facades\Route;
 
 //Route ke halaman utama (home)
 Route::get('/', function () {
-    echo "hallo nama saya seven";
+    echo "Hallo, Nama Saya Pak JR";
     //return view('welcome');
 });
-
 //Route ke halaman alamat
-Route::get('/alamat', function () {
+Route::get('/alamat', function(){
     echo "Jalan Rajawali 14. Palembang";
 });
 
-//Route ke halaman alamat
-Route::get('/path1/path2/detail', function () {
-    echo "Jalan Rajawali 14. Palembang";
+//Route ke halaman path1/path2/detail
+Route::get('/path1/path2/detail', function(){
+    echo "Jalan Rajawali 14";
     echo "<br>";
     echo "Rt. 01 Rw. 02";
     echo "<br>";
@@ -41,7 +40,7 @@ Route::get('/user3/{name?}', function($name = 'Tamu'){
     echo "User Name: " . $name;
 });
 
-//Route Dinamis degnan parameter nama dan id
+//Route Dinamis dengan parameter nama dan id
 Route::get('/user4/{id}/{name}', function($id, $name){
     echo "User ID: " . $id;
     echo "<br>";
@@ -49,7 +48,7 @@ Route::get('/user4/{id}/{name}', function($id, $name){
 });
 
 //Router dengan metode POST
-Route::get('/simpan', function(){
+Route::post('/simpan', function(){
     echo "Data berhasil disimpan";
 });
 
@@ -63,29 +62,60 @@ Route::patch('/update2/{id}', function($id){
     echo "Data berhasil diperbarui dengan ID: " . $id;
 });
 
-//Router dengan metode PUT
+//Router dengan metode DELETE
 Route::delete('/hapus/{id}', function($id){
     echo "Data berhasil dihapus dengan ID: " . $id;
 });
 
-//Router untuk menampilkan halaman test_method
+//Route untuk menampilkan halaman test_method
 Route::get('/test-method', function(){
     return view('test_method');
 });
 
-route::get('/produk/',function(){
-    return view('produk.index');
+//Manampilkan halaman profil
+Route::get('/profil', function(){
+    return view("profile");
 });
 
-route::get('/produk/create',function(){
-    return view('produk.create');
-});
-route::get('/produk/search',function(){
-    return view('produk.search');
-});
-route::get('/produk/detail',function(){
-    return view('produk.detail');
-});
-use app/http/controlers/productcontroler;
-route::resource('/product',productcontroler :: class);
-route::get('/produk/search',productControler::class.'@search');
+//Gunakan . untuk memisahkan folder dgn view
+// Route::get('/detailproduk', function(){
+//     return view("produk.detail");
+// });
+
+//mengirim data ke view
+// Route::get('/detailproduk/{name}', function($name){
+//     return view("produk.detail", 
+//         ['product_name' => $name, 
+//         'id'=> 101, 
+//         'color' => 'Silver',
+//         'stock' => 12
+//         ]
+//     );
+// });
+
+// Route::get('/produk/', function(){
+//     return view('produk.index');
+// });
+// Route::get('/produk/create', function(){
+//     return view('produk.create');
+// });
+// Route::get('/produk/search', function(){
+//     return view('produk.search');
+// });
+// Route::get('/produk/detail', function(){
+//     return view('produk.detail');
+// });
+
+use App\Http\Controllers\ProductController;
+//php artisan make:controller ProductController --resource
+Route::resource('/produk', ProductController::class);
+Route::get('/produk/search', ProductController::class.'@search');
+
+//Suplier
+// Route::get('/supplier/', function(){
+//     return view('supplier.index');
+// });
+
+//php artisan make:controller SupplierController --resource
+use App\Http\Controllers\SupplierController;
+Route::resource('/supplier', SupplierController::class);
